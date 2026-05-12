6901ad8fe77142b488a746fb6748eed94eb36f67
Jon <akuras@gmx.de>
2026-05-12 13:52:05 +0200

    Update versionName to '1.8' in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 34
         targetSdk = 35
         versionCode = ciVersionCode ?: 3
-        versionName = ciVersionName ?: "1.7.2"
+        versionName = ciVersionName ?: "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
