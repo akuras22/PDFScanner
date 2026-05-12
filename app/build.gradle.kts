0b91b8ce8743d31517b506c29f558e09a23c885c
Jon <akuras@gmx.de>
2026-05-12 20:28:05 +0200

    Update versionName to 1.9.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 34
         targetSdk = 35
         versionCode = ciVersionCode ?: 4
-        versionName = ciVersionName ?: "1.9.2"
+        versionName = ciVersionName ?: "1.9.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
