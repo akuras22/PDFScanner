7dbff71bf39a42a2f2ff3893e8b1ea0044c18507
Jon <akuras@gmx.de>
2026-05-12 21:30:28 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 34
         targetSdk = 35
         versionCode = ciVersionCode ?: 4
-        versionName = ciVersionName ?: "1.9.4"
+        versionName = ciVersionName ?: "1.9.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
